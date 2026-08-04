3b64293b31efa9cc540dbcd07006ee0a34e30701
mkghosh <mithun.k.ghose@gmail.com>
2026-08-04 13:20:40 +0600

    Changed Keymap Group Def
    Remove groups definition from java.lua and spring.lua keymap files and
    added the groups to utils.which-key.lua

--- lua/utils/which-key.lua
+++ lua/utils/which-key.lua
@@ -4,15 +4,20 @@ function M.setup()
     local wk = require("which-key")
 
     wk.add({
-        { "<leader>e", group = "Explorer" },
-        { "<leader>f", group = "Find" },
-        { "<leader>g", group = "Git" },
-        { "<leader>h", group = "Harpoon" },
-        { "<leader>l", group = "LSP" },
-        { "<leader>t", group = "Terminal" },
-        { "<leader>x", group = "Trouble" },
-        { "<leader>j", group = "Java" },
-        { "<leader>z", group = "Folds" },
+        { "<leader>e",  group = "Explorer" },
+        { "<leader>f",  group = "Find" },
+        { "<leader>g",  group = "Git" },
+        { "<leader>h",  group = "Harpoon" },
+        { "<leader>l",  group = "LSP" },
+        { "<leader>t",  group = "Terminal" },
+        { "<leader>x",  group = "Trouble" },
+        { "<leader>j",  group = "Java" },
+        { "<leader>jc", group = "Workspace" },
+        { "<leader>jp", group = "Project" },
+        { "<leader>jr", group = "Refactor" },
+        { "<leader>jt", group = "JavaTest" },
+        { "<leader>js", group = "Spring" },
+        { "<leader>z",  group = "Folds" },
     })
 end
 
