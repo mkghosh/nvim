a356712dc59e61304d162fa4b329538026e4c343
mkghosh <mithun.k.ghose@gmail.com>
2026-08-30 12:22:21 +0600

    Updated Bundles.lua
    Updated bundles.lua to be able to run java tests successfully.

--- lua/lsp/servers/jdtls/bundles.lua
+++ lua/lsp/servers/jdtls/bundles.lua
@@ -4,6 +4,11 @@ local M = {}
 
 local mason = path.mason("packages")
 
+local java_test_server =
+    vim.fn.expand(
+        "~/.local/share/nvim/java-test-0.46.0/extension/server"
+    )
+
 local function glob(pattern)
     return vim.fn.glob(pattern, false, true)
 end
@@ -26,17 +31,85 @@ function M.find()
 
     --------------------------------------------------------------------------
     -- Java Test
+    --
+    -- Upstream vscode-java-test 0.46.0
+    --
+    -- IMPORTANT:
+    -- Do not use Mason's java-test package here.
+    --
+    -- Mason currently installs the 0.45.0 registry entry but the package
+    -- contains the older 0.43.1 server/JAR dependency set.
+    --
+    -- Upstream 0.46.0 ships:
+    --   ASM       9.10.1
+    --   ASM Tree  9.10.1
+    --   ASM Commons 9.10.1
+    --   JaCoCo    0.8.15
     --------------------------------------------------------------------------
 
-    for _, jar in ipairs(
-        glob(mason .. "/java-test/extension/server/*.jar")
-    ) do
-        local name = vim.fn.fnamemodify(jar, ":t")
+    local java_test_jars = {
+        "com.microsoft.java.test.plugin-0.43.1.jar",
 
-        if name ~= "com.microsoft.java.test.runner-jar-with-dependencies.jar"
-            and name ~= "jacocoagent.jar"
-        then
+        "junit-jupiter-api_5.14.4.jar",
+        "junit-jupiter-api_6.0.1.jar",
+
+        "junit-jupiter-engine_5.14.4.jar",
+        "junit-jupiter-engine_6.0.1.jar",
+
+        "junit-jupiter-migrationsupport_5.14.4.jar",
+
+        "junit-jupiter-params_5.14.4.jar",
+        "junit-jupiter-params_6.0.1.jar",
+
+        "junit-platform-commons_1.14.4.jar",
+        "junit-platform-commons_6.0.1.jar",
+
+        "junit-platform-engine_1.14.4.jar",
+        "junit-platform-engine_6.0.1.jar",
+
+        "junit-platform-launcher_1.14.4.jar",
+        "junit-platform-launcher_6.0.1.jar",
+
+        "junit-platform-runner_1.14.4.jar",
+
+        "junit-platform-suite-api_1.14.4.jar",
+        "junit-platform-suite-api_6.0.1.jar",
+
+        "junit-platform-suite-commons_1.14.4.jar",
+
+        "junit-platform-suite-engine_1.14.4.jar",
+        "junit-platform-suite-engine_6.0.1.jar",
+
+        "junit-vintage-engine_5.14.4.jar",
+
+        "org.apiguardian.api_1.1.2.jar",
+
+        "org.eclipse.jdt.junit4.runtime_1.4.0.v20251113-1434.jar",
+        "org.eclipse.jdt.junit5.runtime_1.2.0.v20251113-1434.jar",
+        "org.eclipse.jdt.junit6.runtime_1.0.0.v20251112-1701.jar",
+
+        "org.jacoco.core_0.8.15.202606040825.jar",
+
+        "org.objectweb.asm.commons_9.10.1.jar",
+        "org.objectweb.asm.tree_9.10.1.jar",
+        "org.objectweb.asm_9.10.1.jar",
+
+        "org.opentest4j_1.3.0.jar",
+    }
+
+    for _, name in ipairs(java_test_jars) do
+        local jar = vim.fs.joinpath(
+            java_test_server,
+            name
+        )
+
+        if vim.fn.filereadable(jar) == 1 then
             table.insert(bundles, jar)
+        else
+            vim.notify(
+                "Java Test bundle missing: " .. jar,
+                vim.log.levels.WARN
+            )
         end
     end
 
